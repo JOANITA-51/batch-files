8359e719af486d65d9117034a42e512ab5afbb2f
JOANITA-51 <nakityojoanita51@gmail.com>
2021-05-25 06:33:00 +0300

    first batch files

---
+++ first.bat
@@ -0,0 +1,6 @@
+ECHO OFF
+title This is your first batch script!
+ECHO Congratulations! Your first batch file evecuted successfully.
+echo The Computer Name is %ComputerName%
+echo Current time and date is %time% %date%
+PAUSE
---
+++ lunch.bat
@@ -0,0 +1,5 @@
+ECHO OFF
+title lunch is here
+ECHO it is lunch time
+echo Current time and date is %time% %date%
+PAUSE
